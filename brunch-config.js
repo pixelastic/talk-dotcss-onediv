8ca399ee5ee62d56ee97a2d1b4c616fb59807ff0
Pixelastic <tim@pixelastic.com>
2016-11-27 02:28:43 +0100

    Adding slides

---
+++ brunch-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  paths: {
+    watched: ['app/assets', 'app/styles/', 'app/javascripts/']
+  },
+  files: {
+    javascripts: {
+      joinTo: {
+        'app.js': /^app\/javascripts\//
+      }
+    },
+    stylesheets: {joinTo: 'main.css'}
+  },
+  plugins: {
+    sass: {
+      mode: 'native'
+    },
+    babel: {
+      presets: ['es2015'],
+      ignore: [],
+      pattern: /\.js$/
+    }
+  },
+  server: {
+    run: 'yes',
+    port: 5022
+  }
+};
